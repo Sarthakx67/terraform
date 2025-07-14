d1cd5b5e0430df8ab38daa3dba87aa10ca3151c3
sarthak <sarthakx67@gmail.com>
2025-07-14 18:07:20 +0530

    terraform

---
+++ 14-roboshop-infra-user/vpc.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source = "../15-terraform-aws-vpc-advanced"
+  # creating vpc
+  cidr_block = var.cidr_block 
+  common_tags = var.common_tags
+  project_name = var.project_name
+  vpc_tags = var.vpc_tags
+  # creating internet gateway
+  igw_tags = var.igw_tags
+  # craeting public subnet
+  public_subnet_cidr_block = var.public_subnet_cidr_block
+  availability_zone = var.availability_zone
+  # craeting private subnet
+  private_subnet_cidr_block = var.private_subnet_cidr_block
+  #craeting database subnet
+  database_subnet_cidr_block = var.database_subnet_cidr_block
+  #craeting internet gateway
+  #creating public route table
+  
+}
+
