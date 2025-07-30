335e13a726f1f5c8e79e0a0bcbf9c45708349591
sarthak <sarthakx67@gmail.com>
2025-07-31 01:02:49 +0530

    terraform

--- 03-roboshop-infra-standard/06-catalogue/main.tf
+++ 03-roboshop-infra-standard/06-catalogue/main.tf
@@ -33,3 +33,57 @@ resource "aws_launch_template" "catalogue" {
     }
   }
 }
+
+resource "aws_autoscaling_group" "catalogue" {
+  name                      = "${var.project_name}-${var.common_tags.component}"
+  max_size                  = 5
+  min_size                  = 1
+  health_check_grace_period = 300
+  health_check_type         = "ELB"
+  desired_capacity          = 2
+  launch_template {
+    id    = aws_launch_template.catalogue.id
+    version = "$Latest"
+  }
+  vpc_zone_identifier       = split(",",data.aws_ssm_parameter.private_subnet_ids.value)
+
+  tag {
+    key                 = "Name"
+    value               = "Catalogue"
+    propagate_at_launch = true
+  }
+
+  timeouts {
+    delete = "15m"
+  }
+}
+
+resource "aws_autoscaling_policy" "Catalogue" {
+  autoscaling_group_name = aws_autoscaling_group.catalogue.name
+  name                   = "cpu"
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+
+    target_value = 50.0
+  }
+}
+
+resource "aws_lb_listener_rule" "catalogue" {
+  listener_arn = data.aws_ssm_parameter.app_alb_listener_arn.value
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.catalogue_tg.arn
+  }
+
+  condition {
+    host_header {
+      values = ["catalogue.app.stallions.space"]
+    }
+  }
+}
