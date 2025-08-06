3a029074a1b47aa4ca82a053dc240c9c0822e7da
sarthak <sarthakx67@gmail.com>
2025-08-07 00:07:16 +0530

    update

--- 01-terraform-concepts/01-simple-ec2/create-instance.tf
+++ 01-terraform-concepts/01-simple-ec2/create-instance.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "my-choice-name"{
-    ami = var.image_id
+    ami = data.aws_ami.roboshop-ami.id 
     instance_type = var.instance_type
     security_groups = [aws_security_group.allow_all.name]
+    tags = {
+      Name = "Mongodb"
+    }
 }
--- 01-terraform-concepts/01-simple-ec2/variables.tf
+++ 01-terraform-concepts/01-simple-ec2/variables.tf
@@ -1,7 +1,3 @@
-variable "image_id" {
-    type = string
-    default = "ami-0f918f7e67a3323f0"
-}
 variable "instance_type"{
     type = string
     default = "t2.micro"
